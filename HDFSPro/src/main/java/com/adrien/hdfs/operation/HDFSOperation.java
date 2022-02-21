59949d7ea0f5cf70c779cca903fda76a1dba7a17
adrien <you@example.com>
2022-02-21 15:22:34 +0800

    HDFS commit

---
+++ HDFSPro/src/main/java/com/adrien/hdfs/operation/HDFSOperation.java
@@ -0,0 +1,20 @@
+package com.adrien.hdfs.operation;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class HDFSOperation {
+
+    @Test
+    public void mkDir () throws IOException {
+        Configuration configuration = new Configuration();
+        configuration.set("fs.defaultFS","hdfs://nn:9000");
+        FileSystem fileSystem = FileSystem.get(configuration);
+        fileSystem.mkdirs(new Path("/adrien/dir001"));
+        fileSystem.close();
+    }
+}
